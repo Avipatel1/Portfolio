7490e73ffb39d89c5c409207abde60199b36799b
Avi Patel <avixpatel@hotmail.com>
2025-09-18 19:31:15 -0400

    created a basic template w links

--- main/views.py
+++ main/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Project
 
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def projects(request):
+    projects = Project.objects.all()
+    return render(request, 'main/projects.html', {
+        'projects': projects,
+    })
+
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,4 +21,6 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path("about/", views.about, name="about"),
+    path("projects/", views.projects, name="projects"),
 ]
