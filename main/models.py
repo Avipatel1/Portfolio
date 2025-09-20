acb8935ace921f3c1367981c0f6c7aa0e7961313
Avi Patel <avixpatel@hotmail.com>
2025-09-20 15:31:53 -0400

    installed tailwind properly

--- main/models.py
+++ main/models.py
@@ -5,6 +5,8 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     link = models.URLField(blank=True)
+    github_link = models.URLField(blank=True, null=True)
+    image = models.ImageField(upload_to='projects/')
 
     def __str__(self):
         return self.title
