d3a18377fcab53e0c58f7c1d0284700d6a30008a
Avi Patel <avixpatel@hotmail.com>
2025-09-21 12:28:23 -0400

    update to home page

--- main/models.py
+++ main/models.py
@@ -6,7 +6,11 @@ class Project(models.Model):
     description = models.TextField()
     link = models.URLField(blank=True)
     github_link = models.URLField(blank=True, null=True)
-    image = models.ImageField(upload_to='projects/')
+    image = models.ImageField(upload_to='projects/', blank=True)
+    tags = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.title
+    
+    def tag_list(self):
+        return [tag.strip() for tag in self.tags.split(",") if tag.strip()]
